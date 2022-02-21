d59ac66762f38d0e2b223f8c201da6fab97e72c5
PLSeuJ <jonathanschumann@posteo.de>
2022-02-21 19:23:46 +0100

    initialising git

---
+++ rain_costa_rica.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+rain_costa_rica: Make a few need plots of the trend of rainfall in CR.
+
+This script shall make a timeseries of rainfall in costa rica, to investigate
+the question if a 4 day periode of no rain is a common thing in january/febuary
+or if other effects naming: La Niña or Climate Change have an ifluence.
+
+Created on Mon Jan 30 22:08:48 2022
+
+@author: jonathan
+"""
+import xarray
+
+filename = 'maybe even a list?'
+path = 'this/should/not/be/hardcoded'
